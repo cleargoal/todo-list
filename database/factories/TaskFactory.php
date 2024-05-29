ba9a80dddab722b08a1e58583340bc01c1b00180
yefrem <cleargoal01@gmail.com>
2024-05-29 21:53:10 +0300

    fix TaskFactory.php 'status' -> 'completed_at' logic

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -21,15 +21,16 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         $created = Carbon::now()->addDays(rand(-90, -15));
+        $status = $this->faker->randomElement(StatusEnum::cases());
         return [
             'user_id' => rand(1,5),
             'parent_id' => Arr::random([null, rand(1,5), rand(3,9), rand(8,20), rand(9,25), rand(11,40)]),
-            'status' => $this->faker->randomElement(StatusEnum::cases()),
+            'status' => $status,
             'priority' => $this->faker->randomElement(PriorityEnum::cases()),
             'title' => $this->faker->realText(100, 3),
             'description' => $this->faker->realText(1000, 3),
             'created_at' => $created,
-            'completed_at' => Arr::random([null, $this->faker->dateTimeThisMonth()]),
+            'completed_at' => $status->value === 'done' ? $this->faker->dateTimeThisMonth() : null,
         ];
     }
 }
