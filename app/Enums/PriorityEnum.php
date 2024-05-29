a0717fb1725aee75d13068c43897f02902740629
yefrem <cleargoal01@gmail.com>
2024-05-29 19:52:13 +0300

    create Enums

---
+++ app/Enums/PriorityEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum PriorityEnum: string
+{
+    case HIGH = '1';
+    case MIDHIGH = '2';
+    case MID = '3';
+    case MIDLOW = '4';
+    case LOW = '5';
+}
---
+++ app/Enums/StatusEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum StatusEnum: string
+{
+    case TODO = 'To Do';
+    case DONE = 'done';
+}
