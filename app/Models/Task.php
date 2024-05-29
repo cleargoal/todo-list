730dbef405072939040f4f0d07c8c8f2217dc9f9
yefrem <cleargoal01@gmail.com>
2024-05-29 19:55:55 +0300

    cast Enums in model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\PriorityEnum;
+use App\Enums\StatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
@@ -11,4 +13,9 @@ class Task extends Model
     use HasFactory, Searchable;
 
     protected $fillable = ['user_id', 'parent_id', 'status', 'priority', 'title', 'description', 'created_at', 'completed_at',];
+
+    protected $casts = [
+        'status' => StatusEnum::class,
+        'priority' => PriorityEnum::class,
+    ];
 }
