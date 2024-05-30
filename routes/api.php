fb96caf5d4b054d1d7ffdceb943cbcd9c20a6a6a
yefrem <cleargoal01@gmail.com>
2024-05-30 14:03:37 +0300

    install 'sanctum' auth package

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 
 Route::middleware(['api',])->group(function () {
     Route::apiResource('/tasks', TaskController::class);
