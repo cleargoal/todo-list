fbfa8f7fad40923be1897b3241821e0181e9f00a
yefrem <cleargoal01@gmail.com>
2024-05-30 15:19:38 +0300

    add authentication routes to 'api.php'

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->get('/auth-user', [AuthController::class, 'authUser']);
 
-
-Route::middleware(['api',])->group(function () {
+Route::middleware(['api','auth:sanctum',])->group(function () {
     Route::apiResource('/tasks', TaskController::class);
 });
