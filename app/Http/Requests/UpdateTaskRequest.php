00d7ae08d6fcf33fcedd73bc93826bedf37d44b1
yefrem <cleargoal01@gmail.com>
2024-06-02 16:55:21 +0300

    create UpdateTaskRequest.php

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -11,7 +11,7 @@ class UpdateTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'status' => 'sometimes|string|max:5',
+            'priority' => 'sometimes|string|max:1',
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string|max:10000',
         ];
     }
 }
