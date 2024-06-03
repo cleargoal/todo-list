22bcdf0812a554ebe9604b5dabeab6dde229ed50
yefrem <cleargoal01@gmail.com>
2024-06-03 10:40:07 +0300

    create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Start User',
+            'email' => 'start-user@mail.org',
+        ]);
+        User::factory(5)->create();
+    }
+}
