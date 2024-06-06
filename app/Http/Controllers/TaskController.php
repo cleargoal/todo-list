91fe9392554e7c2d230aa8e9ef02eb9b1f53f6e4
yefrem <cleargoal01@gmail.com>
2024-06-06 16:01:39 +0300

    optimize code of TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,51 +2,93 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MarkTaskDoneRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskIndexResource;
 use App\Http\Resources\TaskShowResource;
 use App\Models\Task;
+use App\Repositories\TaskRepository;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
+     * Display a listing of the tasks
+     * @param TaskRepository $repository
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(TaskRepository $repository): AnonymousResourceCollection
     {
-        //
+        return TaskIndexResource::collection($repository->getTasksByAuthor());
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created Task in storage/DB
+     * @param StoreTaskRequest $request
+     * @param TaskRepository $repository
+     * @return TaskShowResource
      */
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request, TaskRepository $repository): TaskShowResource
     {
-        //
+        return new TaskShowResource($repository->create($request->validated()));
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified Task
+     * @param Task $task
+     * @return TaskShowResource|JsonResponse
      */
-    public function show(Task $task): TaskShowResource
+    public function show(Task $task): TaskShowResource|JsonResponse
     {
         return new TaskShowResource($task);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified Task in DB
+     * @param UpdateTaskRequest $request
+     * @param Task $task
+     * @param TaskRepository $repository
+     * @return JsonResponse|TaskShowResource
      */
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(UpdateTaskRequest $request, Task $task, TaskRepository $repository): JsonResponse|TaskShowResource
     {
-        //
+        return new TaskShowResource($repository->update($task, $request->validated()));
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified Task from DB
+     * @param Task $task
+     * @param TaskRepository $repository
+     * @return JsonResponse
      */
-    public function destroy(Task $task)
+    public function destroy(Task $task, TaskRepository $repository): JsonResponse
     {
-        //
+        return response()->json($repository->delete($task));
     }
+
+    /**
+     * Mark Task as 'done'
+     * @param MarkTaskDoneRequest $request
+     * @param Task $task
+     * @param TaskRepository $repository
+     * @return TaskShowResource
+     */
+    public function markTaskDone(MarkTaskDoneRequest $request, Task $task, TaskRepository $repository): TaskShowResource
+    {
+        return new TaskShowResource($repository->update($task, $request->validated()));
+    }
+
+    /**
+     * Get tasks collection by filters
+     * @param
+     */
+    public function getFilteredCollection($filters)
+    {
+
+    }
+
 }
