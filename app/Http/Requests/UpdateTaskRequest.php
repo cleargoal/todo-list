59856cb93184f4951861509951a2a5696f27acd2
yefrem <cleargoal01@gmail.com>
2024-06-08 16:47:55 +0300

    remove 'status' from UpdateTaskRequest.php

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -25,11 +25,10 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'status' => [Rule::enum(StatusEnum::class), 'sometimes', 'required_without_all:priority,title,description'],
-            'priority' => [Rule::enum(PriorityEnum::class), 'sometimes', 'required_without_all:status,title,description'],
             'parent_id' => 'sometimes|integer|exclude',
             'title' => 'sometimes|string|max:255|required_without_all:priority,status,description',
             'description' => 'sometimes|string|max:10000|required_without_all:priority,title,status',
+            'priority' => [Rule::enum(PriorityEnum::class), 'sometimes', 'required_without_all:status,title,description'],
         ];
     }
 }
