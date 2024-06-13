c0a7227b2528fb6de6f95e48c8db7009c68d30ba
yefrem <cleargoal01@gmail.com>
2024-06-13 18:23:33 +0300

    add method for sorting to TaskRepository.php

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Repositories;
+
 use App\Dto\TaskCreateDto;
+use App\Dto\TaskFiltersDto;
 use App\Dto\TaskUpdateDto;
 use App\Enums\StatusEnum;
 use App\Models\Task;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
 
 class TaskRepository
 {
@@ -94,6 +98,45 @@ class TaskRepository
         $task->status = StatusEnum::DONE;
         $task->save();
         return $task->fresh();
+    }
+
+    /**
+     * Get filtered user's tasks
+     * @param int $userId
+     * @param TaskFiltersDto $filters
+     * @return Collection<Task>
+     */
+    public function getFilteredTasks(int $userId, TaskFiltersDto $filters): Collection
+    {
+        $query = Task::where('user_id', $userId);
+
+        foreach ($filters as $key => $filter) {
+            if ($filter !== null) {
+                $query->where($key, $filter);
+            }
+        }
+
+        return $query->get();
+    }
+
+    /**
+     * Get sorted user's tasks
+     *
+     * @param int $userId
+     * @param array $parameters
+     * @return Collection<Task>
+     */
+    public function getSortedTasks(int $userId, array $parameters): Collection
+    {
+        $query = Task::where('user_id', $userId);
+
+        foreach ($parameters as $key => $param) {
+            if ($param !== null) {
+                $query->orderBy($key, $param);
+            }
+        }
+
+        return $query->get();
     }
 
 }
