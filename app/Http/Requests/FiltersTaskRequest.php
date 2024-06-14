7210826653f258f76834325d80c417c92ac7b6a2
yefrem <cleargoal01@gmail.com>
2024-06-14 09:15:56 +0300

    remove from FormRequests a redundant method 'authorize'

--- app/Http/Requests/FiltersTaskRequest.php
+++ app/Http/Requests/FiltersTaskRequest.php
@@ -11,14 +11,6 @@ use Illuminate\Validation\Rule;
 class FiltersTaskRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array|string>
--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -10,14 +10,6 @@ use Illuminate\Validation\Rule;
 class UpdateTaskRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array|string>
