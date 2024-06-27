3961bd493a500f6a248b057f786db9c6186321f2
yefrem <cleargoal01@gmail.com>
2024-06-27 14:48:50 +0300

    fix status code of TaskDeletionException.php

--- app/Exceptions/TaskDeletionException.php
+++ app/Exceptions/TaskDeletionException.php
@@ -11,7 +11,7 @@ class TaskDeletionException extends Exception
     protected $message;
     protected $code;
 
-    public function __construct($message = 'Task deletion error', $code = 409)
+    public function __construct($message = 'Task deletion error', $code = 400)
     {
         parent::__construct($message, $code);
     }
