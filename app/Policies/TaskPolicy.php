49ac0f64d1c059eb806ec9dc4cb60f62e26f7ec4
yefrem <cleargoal01@gmail.com>
2024-06-27 15:57:42 +0300

    make 1 unified method 'own' for user actions authorization

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -16,31 +16,8 @@ class TaskPolicy
      * @param Task $task
      * @return bool
      */
-    public function view(User $user, Task $task): bool
+    public function own(User $user, Task $task): bool
     {
         return $task->user_id === $user->id;
     }
-
-    /**
-     * Determine whether the user can update the model.
-     * @param User $user
-     * @param Task $task
-     * @return bool
-     */
-    public function update(User $user, Task $task): bool
-    {
-        return $task->user_id === $user->id;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     * @param User $user
-     * @param Task $task
-     * @return bool
-     */
-    public function delete(User $user, Task $task): bool
-    {
-        return $task->user_id === $user->id;
-    }
-
 }
