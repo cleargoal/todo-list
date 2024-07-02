57b841eb2befbba87d6dd7d2ca97e72a13ea1d97
yefrem <cleargoal01@gmail.com>
2024-07-02 19:40:15 +0300

    add ServiceProvider of 'L5Swagger' package to providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,9 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 return [
     App\Providers\AppServiceProvider::class,
     Laravel\Scout\ScoutServiceProvider::class,
+    L5Swagger\L5SwaggerServiceProvider::class,
 ];
