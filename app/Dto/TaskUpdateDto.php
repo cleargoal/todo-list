c43f6b13886bc90a339f5af8d0e4038acf6fc42f
yefrem <cleargoal01@gmail.com>
2024-07-03 09:35:10 +0300

    adjust TaskUpdateDto.php

--- app/Dto/TaskUpdateDto.php
+++ app/Dto/TaskUpdateDto.php
@@ -6,23 +6,22 @@ namespace App\Dto;
 
 use App\Enums\PriorityEnum;
 
-readonly class TaskUpdateDto
+class TaskUpdateDto
 {
-    public function __construct(
-        public ?int          $parentId,
-        public ?string       $title,
-        public ?string       $description,
-        public ?PriorityEnum $priority,
-    ) {
-    }
+    public ?int $parentId;
+    public ?string $title;
+    public ?string $description;
+    public ?PriorityEnum $priority;
 
-    public static function fromArray(array $data): TaskUpdateDto
-    {
-        return new self(
-            parentId: $data['parent_id'],
-            title: $data['title'],
-            description: $data['description'],
-            priority: $data['priority'],
-        );
+    public function __construct(
+        ?int $parentId = null,
+        ?string $title = null,
+        ?string $description = null,
+        ?PriorityEnum $priority = null
+    ) {
+        $this->parentId = $parentId;
+        $this->title = $title;
+        $this->description = $description;
+        $this->priority = $priority;
     }
 }
