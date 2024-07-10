9b01c4979bb011b004c7134fd588d8837d0a4d7c
yefrem <cleargoal01@gmail.com>
2024-07-10 21:24:54 +0300

    add handling exceptions to bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,19 @@
 
 declare(strict_types = 1);
 
+use App\Exceptions\TaskAlreadyDoneException;
+use App\Exceptions\TaskDeletionException;
+use App\Exceptions\TaskHasUncompletedChildrenException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +27,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (Exception $e): Response {
+            Log::info('Denied', ['instance' => get_class($e), 'exception' => $e->getMessage(), $e->getCode()]);
+            Log::info('Denied.code', [$e->getCode()]);
+            $statusCode = Response::HTTP_FORBIDDEN;
+            if ($e instanceof TaskAlreadyDoneException) {
+                $statusCode = Response::HTTP_CONFLICT;
+            }
+            return response()->json(['error' => $e->getMessage(),], $statusCode);
+        });
     })->create();
