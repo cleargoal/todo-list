11c102a19fa342659a35d35aee118a0431dc2721
yefrem <cleargoal01@gmail.com>
2024-07-11 15:27:13 +0300

    refactoring TaskSeeder.php to create the deep tree for the main user

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -1,12 +1,13 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Database\Seeders;
 
 use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class TaskSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        Task::factory(50)->create();
+        for ($i = 0; $i < 40; $i++) {
+            $task = Task::factory()->create([
+                'user_id' => 1,
+                'parent_id' => $i === 0 ? null : rand(1, $i),
+            ]);
+        }
+
+        Task::factory(60)->create();
     }
 }
